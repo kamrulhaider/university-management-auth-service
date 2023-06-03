bcacd5548157f3408f9bc9becbeb639e11a4a625
kamrulhaider <kamrulhaider.ssb@gmail.com>
2023-06-03 21:20:54 +0600

    Added prettier husky and lint stage

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,17 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config";
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config'
 
 async function main() {
   try {
-    await mongoose.connect(config.databaseUrl as string);
-    console.log("Database connected");
+    await mongoose.connect(config.databaseUrl as string)
+    console.log('Database connected')
     app.listen(config.port, () => {
-      console.log(`App listening on port ${config.port}`);
-    });
+      console.log(`App listening on port ${config.port}`)
+    })
   } catch (err) {
-    console.log(err);
+    console.log(err)
   }
 }
 
-main();
+main()
